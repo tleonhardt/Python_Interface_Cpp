2ad29f03e7ee8f3af61adb33aff47805061662f5
Todd Leonhardt <todd.leonhardt@gmail.com>
2017-04-12 23:33:03 -0400

    Trying to install pybind11 from GitHub
    It still doesn't work.

--- pybind11/build_pybind11.sh
+++ pybind11/build_pybind11.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 # Path to pybind11 headers
-ANACONDA=~/anaconda3
-# PYBIND11_INCLUDE=${ANACONDA}/include/python3.6m
-PYBIND11_INCLUDE=${ANACONDA}//envs/py27/include/python2.7/
+PYBIND11_INCLUDE=/usr/local/include
 
 # On Linux and Mac OS, this example can be compiled using the following command (for Windows, use CMake)
 c++ -O3 -shared -std=c++11 -Wall -fPIC -I ${PYBIND11_INCLUDE} `python-config --cflags --ldflags` fibonacci.cpp -o fibonacci.so
